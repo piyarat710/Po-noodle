7a85782a4ac7ff5ca2c22c8b9324799d2961670d
piyarat710 <nondata78@gmail.com>
2026-01-16 20:32:07 +0700

    fix vercel spa routing

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const isLogin = localStorage.getItem("staffLogin") === "true";
+  const isLogin = localStorage.getItem("isStaff") === "true";
 
   if (!isLogin) {
-    return <Navigate to="/staff-login" replace />;
+    return <Navigate to="/login" replace />;
   }
 
   return children;
